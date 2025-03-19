11e812d2c67189aa694d6ba182541d21b82afec6
Azmeer Hassan Ammad <azmeerhassanammad@gmail.com>
2025-03-19 13:34:45 +0500

    Handled enter key for sending messages and created message element with dynamic classes and return it

---
+++ AI Chatbot/script.js
@@ -0,0 +1,22 @@
+const messageInput = document.querySelector(".message-input");
+
+//Created message element with dynamic classes and return it
+const createMessageElement = (content, classes)=>{
+    const div = document.createElement("div");
+    div.classList.add("message", classes);
+    div.innerHTML = content;
+    return div;
+}
+
+const handleOutgoingMessage = (userMessage)=>{
+   const messageContent = `<div class="message-text">${userMessage}</div>`;
+    createMessageElement(messageContent, "user-message")
+}
+// handle enter key press for sending messages
+messageInput.addEventListener("keydown", (e)=>{
+   const userMessage = e.target.value.trim();
+   if(e.key === "Enter" && userMessage)
+   {
+    handleOutgoingMessage(userMessage);
+   }
+})
